86ed45eabb9be0c01fefc489f79bd300f225a3cc
bitue <ashikul.islam_ashik@yahoo.com>
2022-02-04 01:04:19 +0600

    all

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Person:
     def set_name(self, new_name):
         self.name = new_name
     def get_bmi(self):
-        bmi = self.weight // (self.heigth * self.heigth)
+        bmi = self.weight / (self.heigth * self.heigth)
         return  bmi
 
     def wellcome_msg(self):
@@ -25,6 +25,25 @@ b_person = Person('farha', 5, 60)
 print(b_person.wellcome_msg())
 b_person.set_name('nazia')
 print(b_person.get_bmi())
+
+class Student(Person):
+    def __init__(self,name, ht, wt, email, id ) :
+        super().__init__(name,ht,wt)
+        self.email=email
+        self.student_id=id
+
+
+    def get_summary(self):
+        return f"name:{self.get_name()} and the bmi is {self.get_bmi()} and email {self.email}"
+
+
+
+student = Student('ashik', 233, 900, 'ashik@gg.com', 121)
+
+print(student.get_summary())
+
+
+
 
 
 
